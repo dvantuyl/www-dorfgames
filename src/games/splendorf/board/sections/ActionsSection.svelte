d24bdec921cf4acc6fb5284900d6420ab5b654d8
Dwight VanTuyl <dvantuyl@gmail.com>
2021-10-10 16:08:35 -0700

    Splendorf | disable action buttons

--- src/games/splendorf/board/sections/ActionsSection.svelte
+++ src/games/splendorf/board/sections/ActionsSection.svelte
@@ -1,14 +1,13 @@
-<script>
-	import { createEventDispatcher } from 'svelte';
+<script lang="ts">
 	export let game;
 	export let room;
 
 	function handleAction(event) {
 		switch (event.target.value) {
-			case 'resetTurn':
+			case 'GAME.RESET_TURN':
 				game.send('GAME.RESET_TURN');
 				break;
-			case 'endTurn':
+			case 'GAME.END_TURN':
 				game.send('GAME.END_TURN');
 				game.send('GAME.PUBLISH', {
 					callback: room.publishState
@@ -16,20 +15,26 @@
 				break;
 		}
 	}
+
+	$: disabled = (action): boolean => {
+		return !$game.can({ type: action });
+	};
 </script>
 
 <div class="h-full w-full flex justify-around items-center">
 	<button
-		class="bg-gray-400 border-2 border-gray-700 p-2 rounded-lg font-black text-gray-700"
-		value="resetTurn"
+		class="bg-gray-400 border-2 border-gray-700 p-2 rounded-lg font-black text-gray-700 disabled:hidden"
+		value="GAME.RESET_TURN"
 		on:click={handleAction}
+		disabled={disabled('GAME.RESET_TURN')}
 	>
 		RESET TURN
 	</button>
 	<button
-		class="bg-gray-400 border-2 border-gray-700 p-2 rounded-lg font-black text-gray-700"
-		value="endTurn"
+		class="bg-gray-400 border-2 border-gray-700 p-2 rounded-lg font-black text-gray-700 disabled:hidden"
+		value="GAME.END_TURN"
 		on:click={handleAction}
+		disabled={disabled('GAME.END_TURN')}
 	>
 		END TURN
 	</button>
